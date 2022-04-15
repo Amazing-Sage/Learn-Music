506b3391842d80fdd4803a23ac1ebe5789ebb50d
Baker_Virginia <bakev538@hsd.k12.or.us>
2022-04-15 09:38:59 -0700

    project alfa release

---
+++ style.js
@@ -0,0 +1,28 @@
+// nav bar js (3/29)//
+const navSide = () => {
+
+  const burger = document.querySelector('.burger');
+  const navLinks = document.querySelectorAll('.nav-links')
+
+  //toggle nav
+  burger.addEventListner('click', () => {
+
+    if('click'){
+      navLinks.classList.toggle('show');
+    }
+    
+  });
+  
+  //animate links
+  navLinks.forEach((link,index)=> {
+
+    link.style.animation = 'navLinks 0.5s ease forwards $(index/7)s';
+    
+    consol.log`(index/7 + 0.3)s`;
+    
+  });
+  
+  //burger animate (4/04)// 
+  
+  burger.classList.toggle('toggle');
+}
