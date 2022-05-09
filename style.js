fb68d1023bda2ab5e0228999b783375dcc69e16d
Baker_Virginia <bakev538@hsd.k12.or.us>
2022-05-09 08:46:13 -0700

    javascript change

--- style.js
+++ style.js
@@ -1,31 +1,25 @@
 // nav bar js (3/29)//
-const navSide = () => {
+const navActive= document.querySelector('.navActive');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelectorAll('.nav-links');
 
-  const burger = document.querySelector('.burger');
-  const navLinks = document.querySelectorAll('.nav-links')
+// toggle nav
+burger.addEventListner('click', () => {
+  if ('click') {
+    navLinks.classList.toggle('show');
+  }
+});
 
-  //toggle nav
-  burger.addEventListner('click', () => {
+// animate links
+navLinks.forEach((link, index) => {
+  link.style.animation = 'navLinks 0.5s ease forwards $(index/7)s';
 
-    if('click'){
-      navLinks.classList.toggle('show');
-    }
-    
-  });
-  
-  //animate links
-  navLinks.forEach((link,index)=> {
+  consol.log`(index/7 + 0.7)s`;
+});
 
-    link.style.animation = 'navLinks 0.5s ease forwards $(index/7)s';
-    
-    consol.log`(index/7 + 0.7)s`;
-    
-  });
-  
-  //burger animate (4/04)// 
-  
-  burger.classList.toggle('toggle');
-}
+// burger animate (4/04)//
+
+burger.classList.toggle('toggle');
 
 
 
