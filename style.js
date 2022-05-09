746b7ee209354f70ab83861a5f67ae132a09e9bc
Baker_Virginia <bakev538@hsd.k12.or.us>
2022-05-09 09:28:24 -0700

    minor changes

--- style.js
+++ style.js
@@ -1,18 +1,18 @@
 // nav bar js (3/29)//
-const navActive= document.querySelector('.navActive');
+const navActive= document.querySelector('.nav-active');
 const burger = document.querySelector('.burger');
-const navLinks = document.querySelectorAll('.nav-links');
+const menuItem = document.querySelectorAll('.menu-item');
 
 // toggle nav
 burger.addEventListner('click', () => {
   if ('click') {
-    navLinks.classList.toggle('show');
+    menuItem.classList.toggle('show');
   }
 });
 
 // animate links
-navLinks.forEach((link, index) => {
-  link.style.animation = 'navLinks 0.5s ease forwards $(index/7)s';
+menuItem.forEach((link, index) => {
+  link.style.animation = 'menuItem 0.5s ease forwards $(index/7)s';
 
   consol.log`(index/7 + 0.7)s`;
 });
